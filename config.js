458df0874145dafe317f3741e661a9ef281c9de1
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-11-04 17:35:50 -0700

    sequelize query DB

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const sequelize = new Sequelize("sequelize_db", "root", "password", {
   host: "localhost",
   dialect: "mysql",
   port: 3307,
+  logging: console.log,
 });
 
 // sequelize connection to database
--- index.js
+++ index.js
@@ -5,38 +5,19 @@ const express = require("express");
 const mysql = require("mysql2");
 const app = express();
 
-// connect to database using mysql2
-const mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "sequelize_db",
-  port: 3307,
-});
-
-// test mysql connection
-mysqlConnection.connect((err) => {
-  if (err) {
-    console.log("Error connecting to the database");
-    return;
-  }
-
-  console.log("Connected to the database using mysql2");
-});
-
 // import the Department model
 const Department = require("./models/department");
 
 // get all departments
 app.get("/departments", (req, res) => {
-  // find all departments using mysql2
-  mysqlConnection.query("SELECT * FROM departments", (err, rows, fields) => {
-    if (!err) {
-      res.send(rows);
-    } else {
-      res.send(err.message);
-    }
-  });
+  // find all departments using the Department model
+  Department.findAll()
+    .then((results) => {
+      res.status(200).send(results);
+    })
+    .catch((err) => {
+      res.status(err.status).send(err.message);
+    });
 });
 
 // test sequelize connection
