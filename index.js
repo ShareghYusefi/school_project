145760593c0f9ca7b9cc1a1a1901f69eeb55d4bd
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-11-04 17:59:52 -0700

    modularizing department routes

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
 // sequelize connection to database
 const sequelize = require("./config");
 const express = require("express");
+const DepartmentRoutes = require("./routes/department");
 const app = express();
 
 // urlencoded middleware from express
 app.use(express.urlencoded({ extended: true }));
 // extended means that the values can be any type
 
-// import the Department model
-const Department = require("./models/department");
-
-// get all departments
-app.get("/departments", (req, res) => {
-  // find all departments using the Department model
-  Department.findAll()
-    .then((results) => {
-      res.status(200).send(results);
-    })
-    .catch((err) => {
-      res.status(err.status).send(err.message);
-    });
-});
-
-// post a new department
-app.post("/departments", (req, res) => {
-  // create a new department using the Department model
-  Department.create({
-    name: req.body.name,
-  })
-    .then((department) => {
-      res.status(200).send(department);
-    })
-    .catch((err) => {
-      res.status(err.status).send(err.message);
-    });
-});
+// use department routes
+app.use(DepartmentRoutes);
 
 // test sequelize connection
 sequelize
---
+++ routes/department.js
@@ -0,0 +1,34 @@
+// express router function returns a router object
+const router = require("express").Router();
+
+// import the Department model
+const Department = require("../models/department");
+
+// get all departments
+router.get("/departments", (req, res) => {
+  // find all departments using the Department model
+  Department.findAll()
+    .then((results) => {
+      res.status(200).send(results);
+    })
+    .catch((err) => {
+      res.status(err.status).send(err.message);
+    });
+});
+
+// post a new department
+router.post("/departments", (req, res) => {
+  // create a new department using the Department model
+  Department.create({
+    name: req.body.name,
+  })
+    .then((department) => {
+      res.status(200).send(department);
+    })
+    .catch((err) => {
+      res.status(err.status).send(err.message);
+    });
+});
+
+// export the router
+module.exports = router;
